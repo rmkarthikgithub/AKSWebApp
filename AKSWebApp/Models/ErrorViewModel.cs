ecffd31fca692a07d27124fcd019c897a5156c64
rmkarthikgithub <49424321+rmkarthikgithub@users.noreply.github.com>
2023-04-25 21:40:57 +0530

    Initial commit

---
+++ AKSWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AKSWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
